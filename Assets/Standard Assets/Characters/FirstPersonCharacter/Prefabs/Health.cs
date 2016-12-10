3a39a1e1c982e71b145fe09688ece9f5e8835391
Alois <Alois>
2016-12-10 08:06:19 +0100

    Eric

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Prefabs/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int maxhealth = 100;
+    public int health = 100;
+    public int damageAmount;
+    public bool damage = false;
+    public Slider HealthBar;
+  
+
+    // Use this for initialization
+    void Start () {
+        health = maxhealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "DeathMachine")
+        {
+            health -= damageAmount;
+            HealthBar.value = health;
+        }
+        if (collision.gameObject.name == "TeleportA")
+        {
+
+        }
+    }
+
+ }
