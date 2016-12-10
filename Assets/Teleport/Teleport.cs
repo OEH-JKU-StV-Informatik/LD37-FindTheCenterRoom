601558d0df46296efa2d0b81809d11103ab42039
Eric <eschinwald@gmx.at>
2016-12-10 14:55:24 +0100

    sync changes

---
+++ Assets/Teleport/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "TeleportA")
+        {
+            SceneManager.LoadScene("test", LoadSceneMode.Single);
+        }
+    }
+}
