e5c4aa710be9a96e564f32866df46230abb40016
Eric <eschinwald@gmx.at>
2016-12-10 21:26:50 +0100

    git commit --reset-author

--- Assets/Teleport/Teleport.cs
+++ Assets/Teleport/Teleport.cs
@@ -41,11 +41,8 @@ public class Teleport : MonoBehaviour {
      
         if(collision.gameObject.name == "TeleportA")
         { 
-<<<<<<< HEAD
+
             SceneManager.LoadScene(randomString, LoadSceneMode.Single);
-=======
-            SceneManager.LoadScene("RedRoome", LoadSceneMode.Single);
->>>>>>> d5fcb8212ed6e09a2c72a0ed3986d8877d0ccccb
             justTeleported = true;
 
         }
